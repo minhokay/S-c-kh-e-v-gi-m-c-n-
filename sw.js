78566770d69938734ea2fb8dc5b4927a1fb0d031
minhokay <minhtinhk12@gmail.com>
2026-06-05 12:05:37 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,26 @@
 
+const CACHE_NAME = 'health-app-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cachedResponse => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
